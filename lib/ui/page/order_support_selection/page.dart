3288d39e3bd61ab9e4b7a89227419dc59f12daf2
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-02-23 13:31:56 +0900

    add errorMessage

--- lib/ui/page/order_support_selection/page.dart
+++ lib/ui/page/order_support_selection/page.dart
@@ -1,6 +1,8 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:smart_order_app/domain/entity/phrase.dart';
+import 'package:smart_order_app/ui/component/error.dart';
 import 'package:smart_order_app/ui/component/loader.dart';
 import 'package:smart_order_app/ui/component/simple_app_bar.dart';
 import 'package:smart_order_app/ui/page/order_support_selection/options.dart';
@@ -8,7 +10,8 @@ import 'package:smart_order_app/ui/page/phrase/addition.dart';
 import 'package:smart_order_app/useCase/state/scenes.dart';
 
 Widget createMainWidget(List<Phrase> phrases, WidgetRef ref) {
-  return createOptions(phrases, ref);
+  return Column(
+      children: [Expanded(child: Center(child: createOptions(phrases, ref)))]);
 }
 
 class OrderSupportSelectionPage extends ConsumerWidget {
@@ -20,21 +23,20 @@ class OrderSupportSelectionPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final scenesFuture = ref.watch(scenesNotifierProvider);
-    final mainWidget = scenesFuture.when(
-        // TODO: 修正
-        error: (e, s) => const Text(
-              'エラーが発生しました。',
-              style: TextStyle(fontSize: 20),
-            ),
-        loading: () => createProgressIndicator(context),
-        data: (scenes) {
-          final scene = scenes.firstWhere((scene) => scene.scene == sceneName);
-          return createMainWidget(scene.phrases, ref);
-        });
     return Scaffold(
       appBar: SimpleAppBar(title: sceneName),
       body: SafeArea(
-        child: Column(children: [Expanded(child: Center(child: mainWidget))]),
+        child: scenesFuture.when(
+            error: (e, s) => createErrorMessage(),
+            loading: () => createProgressIndicator(context),
+            data: (scenes) {
+              final scene =
+                  scenes.firstWhereOrNull((scene) => scene.scene == sceneName);
+              if (scene == null) {
+                return createErrorMessage();
+              }
+              return createMainWidget(scene.phrases, ref);
+            }),
       ),
       floatingActionButton: scenesFuture.hasValue
           ? FloatingActionButton(
---
+++ lib/ui/style/text_style.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const body = TextStyle(fontSize: 16);
