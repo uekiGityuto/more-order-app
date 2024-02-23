bc14cb6bedc8a024688547c03950a5833dc1a80d
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-02-23 14:54:48 +0900

    add theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:smart_order_app/domain/repository/repository.dart';
 import 'package:smart_order_app/infrastructure/db/dao.dart';
 import 'package:smart_order_app/infrastructure/db/init/db.dart';
+import 'package:smart_order_app/theme.dart';
 import 'package:smart_order_app/ui/page/home.dart';
 
 void main() async {
@@ -29,8 +30,9 @@ class App extends StatelessWidget {
       home: const Home(),
       title: 'SmartOrderApp',
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(backgroundColor: Color(0xFFDBE1FF)),
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2764EF)),
+        appBarTheme: AppBarTheme(
+            backgroundColor: MaterialTheme.lightScheme().primaryContainer),
+        colorSchemeSeed: MaterialTheme.lightScheme().primary,
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
