15224fa3b8c873a0180d1bd096096f9c1f74a3bb
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-03-03 11:22:15 +0900

    add reason to usecase

---
+++ lib/usecase/state/reasons.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'reasons.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$reasonsNotifierHash() => r'b9bf89d1300d0669145ca0c8aa08ac3e2b2a05b5';
+
+/// See also [ReasonsNotifier].
+@ProviderFor(ReasonsNotifier)
+final reasonsNotifierProvider =
+    AsyncNotifierProvider<ReasonsNotifier, List<Reason>>.internal(
+  ReasonsNotifier.new,
+  name: r'reasonsNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$reasonsNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ReasonsNotifier = AsyncNotifier<List<Reason>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
