183686155148a4534a2a985ba7867cf95b524218
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-03-08 01:09:06 +0900

    add policy

--- lib/ui/component/list_tile_navigation.dart
+++ lib/ui/component/list_tile_navigation.dart
@@ -1,52 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-enum NavigationType { push, pushReplacement, pushAndRemoveUntil }
+enum NavigationType { push, pushReplacement, pushAndRemoveUntil, webView }
+
+class NavigationAction {
+  final BuildContext context;
+
+  NavigationAction(this.context);
+
+  Future<void> execute({
+    required NavigationType type,
+    Widget? nextPage,
+    String? webURL,
+  }) async {
+    switch (type) {
+      case NavigationType.webView:
+        if (webURL != null) {
+          await _openWebView(webURL);
+        }
+        break;
+      case NavigationType.push:
+        if (nextPage != null) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => nextPage),
+          );
+        }
+        break;
+      case NavigationType.pushReplacement:
+        if (nextPage != null) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => nextPage),
+          );
+        }
+        break;
+      case NavigationType.pushAndRemoveUntil:
+        if (nextPage != null) {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => nextPage),
+            (_) => false,
+          );
+        }
+        break;
+    }
+  }
+
+  Future<void> _openWebView(String webURL) async {
+    final url = Uri.parse(webURL);
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      print("Can't launch $url");
+    }
+  }
+}
 
 class ListTileOption {
   final String title;
   final bool isContentPaddingZero;
-  final Widget nextPage;
-  final NavigationType navigateType;
+  final NavigationType navigationType;
+  final Widget? nextPage;
+  final String? webURL;
 
   ListTileOption({
     required this.title,
     this.isContentPaddingZero = false,
-    required this.nextPage,
-    this.navigateType = NavigationType.push,
+    this.navigationType = NavigationType.push,
+    this.nextPage,
+    this.webURL,
   });
 }
 
 class ListTileNavigation extends StatelessWidget {
   final ListTileOption option;
-  const ListTileNavigation({Key? key, required this.option}) : super(key: key);
+
+  const ListTileNavigation({
+    Key? key,
+    required this.option,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final navigationAction = NavigationAction(context);
+
     return ListTile(
-        title: Text(option.title),
-        trailing: const Icon(Icons.arrow_forward),
-        contentPadding: option.isContentPaddingZero ? EdgeInsets.zero : null,
-        onTap: () {
-          switch (option.navigateType) {
-            case NavigationType.push:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => option.nextPage),
-              );
-              break;
-            case NavigationType.pushReplacement:
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => option.nextPage),
-              );
-              break;
-            case NavigationType.pushAndRemoveUntil:
-              Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(builder: (_) => option.nextPage),
-                (_) => false,
-              );
-          }
-        });
+      title: Text(option.title),
+      trailing: const Icon(Icons.arrow_forward),
+      contentPadding: option.isContentPaddingZero ? EdgeInsets.zero : null,
+      onTap: () => navigationAction.execute(
+        type: option.navigationType,
+        nextPage: option.nextPage,
+        webURL: option.webURL,
+      ),
+    );
   }
 }
