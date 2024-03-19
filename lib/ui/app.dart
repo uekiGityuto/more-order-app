b537fde66bf042805fcf90214760f344e0144c56
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-03-20 00:37:18 +0900

    remove dark

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -58,53 +58,6 @@ class App extends ConsumerWidget {
         ),
         useMaterial3: true,
       ),
-      // TODO: darkThemeはいらないかも
-      darkTheme: ThemeData(
-        appBarTheme: AppBarTheme(
-            backgroundColor: MaterialTheme.darkScheme().primaryContainer),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: MaterialTheme.darkScheme().primary,
-          error: MaterialTheme.darkScheme().error,
-          surface: MaterialTheme.darkScheme().surface,
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: MaterialTheme.darkScheme().primary,
-            textStyle: TextStyle(
-              color: MaterialTheme.darkScheme().onPrimary,
-              fontSize: 16 * MediaQuery.of(context).textScaleFactor,
-              fontWeight: FontWeight.bold,
-            ),
-            foregroundColor: MaterialTheme.darkScheme().onPrimary,
-            minimumSize: const Size(double.infinity, 50),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-        ),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-            textStyle: TextStyle(
-              color: MaterialTheme.darkScheme().primary,
-              fontSize: 16 * MediaQuery.of(context).textScaleFactor,
-              fontWeight: FontWeight.bold,
-            ),
-            foregroundColor: MaterialTheme.darkScheme().primary,
-            side: BorderSide(
-              color: MaterialTheme.darkScheme().primary,
-            ),
-            minimumSize: const Size(double.infinity, 50),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          fillColor: MaterialTheme.darkScheme().surface,
-          filled: true,
-        ),
-        useMaterial3: true,
-      ),
       debugShowCheckedModeBanner: false,
     );
   }
