a77eb714d72477bee09e4e5af5e8863b7c13668e
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-03-20 12:59:54 +0900

    add navigation on select page

--- lib/ui/component/typography/section_title.dart
+++ lib/ui/component/typography/section_title.dart
@@ -2,19 +2,27 @@ import 'package:flutter/material.dart';
 
 class SectionTitle extends StatelessWidget {
   final String text;
-  const SectionTitle({Key? key, required this.text}) : super(key: key);
+  final Widget? rightContent;
+  const SectionTitle({Key? key, required this.text, this.rightContent})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 12.0),
-      child: Text(
-        text,
-        style: TextStyle(
-          fontSize: 16 * MediaQuery.of(context).textScaleFactor,
-          color: Theme.of(context).colorScheme.primary,
-          fontWeight: FontWeight.bold,
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: TextStyle(
+              fontSize: 16 * MediaQuery.of(context).textScaleFactor,
+              color: Theme.of(context).colorScheme.primary,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          if (rightContent != null) rightContent!,
+        ],
       ),
     );
   }
