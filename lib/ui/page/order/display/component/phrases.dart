dce080c6440011144315568779a5ab920fe0a71f
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-03-20 12:21:21 +0900

    fix icon alignment on display page

--- lib/ui/page/order/display/component/phrases.dart
+++ lib/ui/page/order/display/component/phrases.dart
@@ -3,25 +3,38 @@ import 'package:more_order_app/domain/entity/phrase.dart';
 
 class Phrases extends StatelessWidget {
   final List<Phrase> phrases;
+
   const Phrases({Key? key, required this.phrases}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: phrases
-          .map(
-            (phrase) => ListTile(
-              leading: const Icon(Icons.fiber_manual_record),
-              title: Text(
-                phrase.phrase,
-                style: TextStyle(
-                  fontSize: 24 * MediaQuery.of(context).textScaleFactor,
+      children: phrases.map((phrase) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                // アイコンとテキストがずれるので調整
+                padding: EdgeInsets.only(
+                    top: 6.0 * MediaQuery.of(context).textScaleFactor),
+                child: Icon(Icons.fiber_manual_record,
+                    size: 16 * MediaQuery.of(context).textScaleFactor),
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Text(
+                  phrase.phrase,
+                  style: TextStyle(
+                    fontSize: 24 * MediaQuery.of(context).textScaleFactor,
+                  ),
                 ),
               ),
-              contentPadding: EdgeInsets.zero,
-            ),
-          )
-          .toList(),
+            ],
+          ),
+        );
+      }).toList(),
     );
   }
 }
