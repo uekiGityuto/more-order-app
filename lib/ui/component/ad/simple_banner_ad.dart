bb2df51e87fafc21beaa9b0585dd44ac6e6246f4
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-03-24 10:05:42 +0900

    fix banner ad

---
+++ lib/ui/component/ad/simple_banner_ad.dart
@@ -0,0 +1,80 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class SimpleBannerAd extends StatefulWidget {
+  const SimpleBannerAd({Key? key}) : super(key: key);
+
+  @override
+  State<SimpleBannerAd> createState() => _SimpleBannerAdState();
+}
+
+class _SimpleBannerAdState extends State<SimpleBannerAd> {
+  late BannerAd _bannerAd;
+  bool _isBannerAdLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initBannerAd();
+  }
+
+  void _initBannerAd() {
+    _bannerAd = BannerAd(
+      adUnitId: _getBannerAdUnitId(),
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBannerAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          debugPrint('Ad failed to load: $error');
+          ad.dispose();
+        },
+      ),
+    );
+    _bannerAd.load();
+  }
+
+  String _getBannerAdUnitId() {
+    if (Platform.isAndroid) {
+      return kDebugMode
+          ? 'ca-app-pub-3940256099942544/6300978111'
+          : 'ca-app-pub-8112256294079848/4307421763';
+    } else if (Platform.isIOS) {
+      return kDebugMode
+          ? 'ca-app-pub-3940256099942544/2934735716'
+          : 'ca-app-pub-8112256294079848/4335341920';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  @override
+  void dispose() {
+    _bannerAd.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const double placeholderHeight = 50;
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: _isBannerAdLoaded ? AdSize.banner.height.toDouble() : placeholderHeight,
+      alignment: Alignment.center,
+      child: _isBannerAdLoaded
+          ? AdWidget(ad: _bannerAd)
+          : const SizedBox(
+              height: placeholderHeight,
+              child: Center(
+                child: Text('Loading ad...'),
+              ),
+            ),
+    );
+  }
+}
--- lib/ui/layout/default_layout.dart
+++ lib/ui/layout/default_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:more_order_app/ui/component/ad/simple_banner_ad.dart';
 import 'package:more_order_app/ui/component/app_bar/simple_app_bar.dart';
 import 'package:more_order_app/ui/component/app_bar/simple_bottom_app_bar.dart';
 import 'package:more_order_app/ui/component/drawer/simple_drawer.dart';
@@ -42,11 +43,18 @@ class DefaultLayout extends StatelessWidget {
                 : null),
         drawer: const SimpleDrawer(),
         body: SafeArea(
-          child: Container(
-            padding: effectiveBodyOption.dense
-                ? const EdgeInsets.all(0)
-                : const EdgeInsets.all(16.0),
-            child: body,
+          child: Column(
+            children: [
+              const SimpleBannerAd(),
+              Flexible(
+                child: Container(
+                  padding: effectiveBodyOption.dense
+                      ? const EdgeInsets.all(0)
+                      : const EdgeInsets.all(16.0),
+                  child: body,
+                ),
+              ),
+            ],
           ),
         ),
         bottomNavigationBar: const SimpleBottomAppBar(),
