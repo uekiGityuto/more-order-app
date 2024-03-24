68ab34997db0a7295ef66f7363c55e27421fa2ac
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-03-24 19:53:05 +0900

    fix todo

--- lib/ui/component/error_message.dart
+++ lib/ui/component/error_message.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 
-// TODO: 文字サイズ周りは別途検討
 class ErrorMessage extends StatelessWidget {
   final String message;
-  const ErrorMessage({Key? key, String? message})
-      : message = message ?? 'エラーが発生しました。お手数ですが、アプリの再起動等をお試し下さい。',
-        super(key: key);
+  const ErrorMessage(
+      {Key? key, this.message = "エラーが発生しました。お手数ですが、アプリの再起動等をお試し下さい。"})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        alignment: Alignment.center,
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Text(
-            message,
-            // style: const TextStyle(fontSize: 16),
-          ),
-        ));
+      alignment: Alignment.center,
+      child: Text(message),
+    );
   }
 }
